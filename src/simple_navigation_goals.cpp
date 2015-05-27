4ed58a9ec9cf9a8595bdcb466247183c29bc0446
WillemSelten <WillemSelten>
2015-05-27 13:25:30 +0200

    Added odometry to the code

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -1,15 +1,29 @@
-#include <tf/transform_broadcaster.h>
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
+#include <nav_msgs/Odometry.h>
+
+double x;
+double y;
+double w;
+
+
+void odomMsgs(const nav_msgs::Odometry& odometry)						//callback function for the position of the robot
+{
+	x = odom.pose.pose.position.x;
+	y = odom.pose.pose.position.y;
+	w = odom.twist.twist.linear.z;	
+}
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
 
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
-
+  
+  ros::Subscriber sub = nh.subscribe("odom", 1, &odomMsgs);					//subscribed to odometry msg
+  
   //tell the action client that we want to spin a thread by default
-  MoveBaseClient ac("move_base_simple", true);
+  MoveBaseClient ac("move_base", true);
 
   //wait for the action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
@@ -24,7 +38,7 @@ int main(int argc, char** argv){
 
   goal.target_pose.pose.position.x = 1.0;
   goal.target_pose.pose.position.y = 0.0;
-  goal.target_pose.pose.orientation = tf::createQuaternionMsgFromYaw(M_PI);
+  goal.target_pose.pose.orientation.z = 1.0;
 
   ROS_INFO("Sending goal");
   ac.sendGoal(goal);
@@ -32,9 +46,11 @@ int main(int argc, char** argv){
   ac.waitForResult();
 
   if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-    ROS_INFO("Hooray, the base moved 1 meter forward");
+    ROS_INFO("Hoera, luc broeren is een kneus");
   else
     ROS_INFO("The base failed to move forward 1 meter for some reason");
 
-  return 0;
+  
+  ROS::SpinOnce;
+  
 }
