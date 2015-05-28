569d4edc9c8be84686372537a732d3d0b1915fab
WillemSelten <WillemSelten>
2015-05-28 10:34:08 +0200

    Changed thing

--- src/simple_navigation_goals.cpp
+++ src/simple_navigation_goals.cpp
@@ -1,7 +1,7 @@
 #include <ros/ros.h>
 #include <move_base_msgs/MoveBaseAction.h>
 #include <actionlib/client/simple_action_client.h>
-#include <nav_msgs/Odometry.h>
+#include <move_base_msgs/MoveBaseActionFeedback.h>
 
 double x;
 double y;
@@ -11,11 +11,14 @@ double goal_y;
 double goal_w;
 
 
-void odomMsgs(const nav_msgs::Odometry& odometry)						//callback function for the position of the robot
+void odomMsgs(const move_base_msgs::MoveBaseActionFeedback& feedback)						//callback function for the position of the robot
 {
-	x = odometry.pose.pose.position.x;
-	y = odometry.pose.pose.position.y;
-	w = odometry.twist.twist.linear.z;	
+	x = feedback.feedback.base_position.pose.position.x;
+	ROS_INFO("%d",x);
+	y = feedback.feedback.base_position.pose.position.y;
+	ROS_INFO("%d",y);
+	w = feedback.feedback.base_position.pose.orientation.z;	
+	ROS_INFO("%d",w);
 }
 
 typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
@@ -23,30 +26,33 @@ typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseCl
 int main(int argc, char** argv){
   ros::init(argc, argv, "simple_navigation_goals");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("odom", 1, &odomMsgs);					//subscribed to odometry msg
+  ros::Subscriber sub = nh.subscribe("move_base/feedback", 1, &odomMsgs);					//subscribed to odometry msg
   
   //tell the action client that we want to spin a thread by default
   MoveBaseClient ac("move_base", true);
+  
+  //ros::Rate loop_rate(10);
 
   //wait for the action server to come up
   while(!ac.waitForServer(ros::Duration(5.0))){
     ROS_INFO("Waiting for the move_base action server to come up");
   }
+  
 
-  while (true) {
+  while (ros::ok()) {
 
       //CALCULATIONS
 
-      if (x<1,02){
-          goal_x = 1.52;
-          goal_y = 0,76;
-          goal_w = 0.5 * M_PI;
+      if (x<0.5){
+          goal_x = 1;
+          goal_y = 0.0;
+          goal_w = 0.5 * 3.1415;
       }
 
-      if ((x > 1,02) && (x < 1,12)){
-          goal_x = 3,02;
-          goal_y = 0,76;
-          goal_w = 0.5 * M_PI;
+      if ((x > 0.5) && (x < 0.6)){
+          goal_x = 2.5;
+          goal_y = 0.0;
+          goal_w = 0.5 * 3.1415;
       }
 
       //END CALCULATIONS
@@ -54,7 +60,7 @@ int main(int argc, char** argv){
 
 
 
-
+	  ROS_INFO("Fuck alles");
       move_base_msgs::MoveBaseGoal goal;
 
       //we'll send a goal to the robot to move 1 meter forward
@@ -63,7 +69,7 @@ int main(int argc, char** argv){
 
       goal.target_pose.pose.position.x = goal_x;
       goal.target_pose.pose.position.y = goal_y;
-      goal.target_pose.pose.orientation.z = goal_w;
+      goal.target_pose.pose.orientation.w = goal_w;
 
       ROS_INFO("Sending goal");
       ac.sendGoal(goal);
@@ -71,11 +77,11 @@ int main(int argc, char** argv){
       ac.waitForResult();
 
       if(ac.getState() == actionlib::SimpleClientGoalState::SUCCEEDED)
-        ROS_INFO("Hoera, luc broeren is een kneus");
+        ROS_INFO("Succesfully reached the goal");
       else
-        ROS_INFO("The base failed to move forward 1 meter for some reason");
+        ROS_INFO("Did not reach the goal. Your robot is fucked up");
 
-
-      //ros::spinOnce();
+	  //loop_rate.sleep();
+      
   }
 }
